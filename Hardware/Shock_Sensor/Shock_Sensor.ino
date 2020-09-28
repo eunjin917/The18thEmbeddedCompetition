973f036056c5b33dce78cc6d21954ada0a48f07d
choesuhong <csoohong64@gmail.com>
2020-09-28 14:07:35 +0000

    Update Shock_Sensor.ino

--- Hardware/Shock_Sensor/Shock_Sensor.ino
+++ Hardware/Shock_Sensor/Shock_Sensor.ino
@@ -1,12 +1,10 @@
 //아두이노간에 I2C 통신방식을 이용
 #include <Wire.h>
 
-#define SHOCK 8 //핀 설정(디지털신호 받는 핀)\
+#define SHOCK 8 //핀 설정(디지털신호 받는 핀)
 
 void setup() {
-  Serial.begin(9600);//PC모니터로 센서값을 확인하기위해서 시리얼 통신을 정의해줍니다.
-                      //시리얼 통신을 이용해 PC모니터로 데이터 값을 확인하는 부분은 자주사용되기 때문에
-                      //필수로 습득해야하는 교육코스 입니다.
+  Serial.begin(9600);
   Wire.begin();
   pinMode(SHOCK, INPUT);
 }
@@ -14,8 +12,8 @@ void setup() {
 void loop() {
   //아두이노에 데이터 전송/
   Wire.beginTransmission(8);
-  if (digitalRead(SHOCK) != HIGH) {
-    Wire.write(1);
+  if (digitalRead(SHOCK) != HIGH) { // 충격을 감지했을 때
+    Wire.write(1); // 충격을 받았다고 신호 전송
   }
   Wire.endTransmission();
 }
